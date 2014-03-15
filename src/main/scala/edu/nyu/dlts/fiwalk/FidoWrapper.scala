509712964e8cb041a9db8a1a5d35ab54d4af1529
don mennerich <don.mennerich@gmail.com>
2014-03-15 11:36:09 +0000

    output of kv pairs for fido and clamav

--- src/main/scala/edu/nyu/dlts/fiwalk/FidoWrapper.scala
+++ src/main/scala/edu/nyu/dlts/fiwalk/FidoWrapper.scala
@@ -1,15 +1,27 @@
 package edu.nyu.dlts.fiwalk
 
 import scala.sys.process._
+import scala.util.matching.Regex._
 
 class FidoWrapper(file: java.io.File){
+
   val fido = ("fido " + file.getAbsolutePath) lines_! ProcessLogger(line => ())  
+  val pattern = "^\".*\"$".r
+
   def getPronom(): Unit = {
     val fields = fido(0).split(",")
-    println("pronomPuid: " + fields(2))
-    println("pronomFormatName: " + fields(3))
-    println("pronomSignatureName: " + fields(4))
-    println("pronomMimeType: " + fields(7))
-    println("pronomMatchType: " + fields(8))    
+    println("pronomPuid: " + removeQuotes(fields(2)))
+    println("pronomFormatName: " + removeQuotes(fields(3)))
+    println("pronomSignatureName: " + removeQuotes(fields(4)))
+    println("pronomMimeType: " + removeQuotes(fields(7)))
+    println("pronomMatchType: " + removeQuotes(fields(8)))    
+  }
+
+  def removeQuotes(in: String): String = {
+    if((pattern findAllIn in).isEmpty){
+      in
+    } else {
+      in.substring(1, in.length -1)
+    }
   }
 }
--- src/main/scala/edu/nyu/dlts/fiwalk/Input.scala
+++ src/main/scala/edu/nyu/dlts/fiwalk/Input.scala
@@ -3,6 +3,23 @@ package edu.nyu.dlts.fiwalk
 class Input(path: String){
   val file = new java.io.File(path)
   new FidoWrapper(file).getPronom
+  new VirusScan(file)
+  println("uuid: " + java.util.UUID.randomUUID.toString)
+
+}
+
+class VirusScan(file: java.io.File){
+  import scala.util.matching.Regex._
+  val fis = new java.io.FileInputStream(file)
+  val scanner = new ClamScan("localhost", 3310, 600000000);
+  val result = scanner.scan(fis)
+  val pattern = "FOUND".r
+  if((pattern findAllIn result.getResult).isEmpty){
+    println("virusFound: false")
+  } else {
+    println("virusFound: true")
+    println("VirusSignature: " + result.getSignature)
+  }
 }
 
 object Input{
